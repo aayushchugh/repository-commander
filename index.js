0da52ae82952a27bb11a3ae174501f8075b5d50e
Ayush Chugh <ayushchugh2006@gmail.com>
2022-08-13 15:26:17 +0530

    feat(issue):sparkles:: Automation of work progress label on pull requests and reaction to commands
    * feat(pull_reqeust): :sparkles: WIP label automation on pull request
    
    * feat(issues): :sparkles: bot will react with thumb up emoji on every command

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {
 	addReadyForReviewLabel,
 	addApprovedLabel,
 	addMergedLabel,
+	pullRequestWIPLabelAutomation,
 } = require('./automation/addLabelsOnPullRequest.automation');
 const addLabelToIssueOnClose = require('./automation/addLabelToIssueOnClose.automation');
 const approveCommand = require('./commands/approve.command');
@@ -11,7 +12,8 @@ const mergeCommand = require('./commands/merge.command');
 const WIPCommand = require('./commands/wip.command');
 const { createComment, deleteComment } = require('./helpers/comment.helper');
 const getCommandAndArgs = require('./helpers/getCommandAndArgs.helper');
-const { addLabel } = require('./helpers/label.helper');
+const { addLabel, removeLabel } = require('./helpers/label.helper');
+const { listIssueLabels } = require('./helpers/listLabels.helper');
 
 const availableCommandsMessage = `Available commands are:- 
 						    - **/label** - Add labels to an issue or pull request.
@@ -29,12 +31,28 @@ module.exports = app => {
 	app.on('pull_request.opened', addReadyForReviewLabel);
 	app.on('pull_request_review', addApprovedLabel);
 	app.on('pull_request.closed', addMergedLabel);
+
+	app.on(
+		['pull_request.edited', 'pull_request.labeled'],
+		pullRequestWIPLabelAutomation
+	);
+
 	app.on('issues.closed', addLabelToIssueOnClose);
 
 	/* --------------------------------- ANCHOR Issue commands --------------------------------- */
 	app.on('issue_comment.created', async context => {
 		const { body } = context.payload.comment;
 		const { command, args } = getCommandAndArgs(body);
+		const commentId = context.payload.comment.id;
+
+		if (command[0] === '/' && !context.isBot) {
+			const params = context.issue({
+				comment_id: commentId,
+				content: '+1',
+			});
+
+			context.octokit.reactions.createForIssueComment(params);
+		}
 
 		switch (command) {
 			case '/label':
@@ -56,6 +74,13 @@ module.exports = app => {
 			default:
 				if (command[0] === '/') {
 					if (!context.isBot) {
+						const params = context.issue({
+							comment_id: commentId,
+							content: '-1',
+						});
+
+						context.octokit.reactions.createForIssueComment(params);
+
 						createComment(
 							context,
 							`**${command}** command doesn't exist.
