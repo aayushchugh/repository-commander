15aaca023aae3539db03df35ae97a3d37c2d917e
Ayush Chugh <ayushchugh2006@gmail.com>
2022-08-28 16:41:57 +0530

    fix(pull_request)!::bug: Only `/WIP` command will work now automation is removed (#30)
    * :bug: WIP label is added when title is changed but when WIP label is manually added title is not getting updated
    Fixes #15 pending
    
    * fix(labels): :bug: only `/WIP` command will now work and WIP automation is removed
    
    * docs: :memo: updated readme

--- commands/wip.command.js
+++ commands/wip.command.js
@@ -1,37 +1,58 @@
-const { addLabel, removeLabel } = require('../helpers/label.helper');
-const { listIssueLabels } = require('../helpers/listLabels.helper');
+const { addLabel, removeLabel } = require("../helpers/label.helper");
+const { listIssueLabels } = require("../helpers/listLabels.helper");
 
 const WIPCommand = async context => {
 	const { title } = context.payload.issue || context.payload.pull_request;
 	const labelsFromIssues = await listIssueLabels(context);
 
 	const wipLabel = labelsFromIssues.data.find(
-		label => label.name === ':construction: WIP'
+		label => label.name === ":construction: WIP"
+	);
+	const foundReadyForReviewLabel = labelsFromIssues.data.find(
+		label => label.name === ":mag: Ready for Review"
 	);
 
 	if (
-		title.includes('WIP') ||
-		title.includes('Work In Progress') ||
-		title.includes('work in progress') ||
-		title.includes(':construction:')
+		title.includes("WIP") ||
+		title.includes("Work In Progress") ||
+		title.includes("work in progress") ||
+		title.includes(":construction:")
 	) {
 		const params = context.issue({
-			title: `${context.payload.issue.title.replace(':construction:', '')}`,
+			title: `${context.payload.issue.title.replace(":construction:", "")}`,
 		});
 
 		context.octokit.issues.update(params);
 	}
 
 	if (wipLabel) {
-		return removeLabel(':construction: WIP', context);
+		if (!foundReadyForReviewLabel) {
+			addLabel([":mag: Ready for Review"], context);
+		}
+
+		return removeLabel(":construction: WIP", context);
 	}
 
-	addLabel([':construction: WIP'], context, '383214');
-	const params = context.issue({
-		title: `:construction: ${context.payload.issue.title}`,
-	});
+	if (!wipLabel) {
+		if (foundReadyForReviewLabel) {
+			removeLabel(":mag: Ready for Review", context);
+		}
 
-	context.octokit.issues.update(params);
+		addLabel([":construction: WIP"], context, "383214");
+	}
+
+	if (
+		!title.includes("WIP") ||
+		!title.includes("Work In Progress") ||
+		!title.includes("work in progress") ||
+		!title.includes(":construction:")
+	) {
+		const params = context.issue({
+			title: `:construction: ${context.payload.issue.title}`,
+		});
+
+		context.octokit.issues.update(params);
+	}
 };
 
 module.exports = WIPCommand;
