809e16f8d0814a056b1190cb0982fca4dc3775bd
Ayush Chugh <ayushchugh2006@gmail.com>
2022-09-05 05:43:48 -0700

    feat(issues)::sparkles: Auto request more info in issue with default … (#54)
    feat(issues)::sparkles: Auto request more info in issue with default title or empty body
    Fixes #47

--- src/utils/label.util.ts
+++ src/utils/label.util.ts
@@ -12,13 +12,12 @@ export async function addLabel(names: string[], context: Context, color?: string
 	const labelsFromRepo = await listRepoLabels(context);
 	const labelsFromIssue = await listIssueLabels(context);
 
-	names.forEach((name) => {
+	names.forEach(async (name) => {
 		const labelFromRepo = labelsFromRepo.data.filter((label) => label.name === name);
 
 		const labelFromIssue = labelsFromIssue.data.filter((label) => label.name === name);
 
 		if (labelFromRepo.length === 0) {
-			console.log("if");
 			if (color) createLabel(name, context, color);
 			else createLabel(name, context);
 		}
@@ -28,7 +27,7 @@ export async function addLabel(names: string[], context: Context, color?: string
 				labels: names,
 			});
 
-			context.octokit.issues.addLabels(newLabels);
+			await context.octokit.issues.addLabels(newLabels);
 		}
 	});
 }
@@ -39,10 +38,11 @@ export async function addLabel(names: string[], context: Context, color?: string
  * @param {Context} context Probot context
  * @param {string} color - Label color
  */
-export function createLabel(name: string, context: Context, color?: string) {
+// @ts-ignore
+export async function createLabel(name: string, context: Context, color?: string) {
 	const params = context.repo();
 
-	return context.octokit.issues.createLabel({
+	return await context.octokit.issues.createLabel({
 		owner: params.owner,
 		repo: params.repo,
 		name: name,
@@ -55,8 +55,8 @@ export function createLabel(name: string, context: Context, color?: string) {
  * @param {string} name - Label name
  * @param {Context} context Probot context
  */
-export function removeLabel(name: string, context: Context) {
+export async function removeLabel(name: string, context: Context) {
 	const issue = context.issue({ name });
 
-	return context.octokit.issues.removeLabel(issue);
+	return await context.octokit.issues.removeLabel(issue);
 }
