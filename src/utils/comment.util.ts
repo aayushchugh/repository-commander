606afc1f9db0dd486920e3f2de2766ec03504b9d
Ayush Chugh <ayushchugh2006@gmail.com>
2022-09-05 05:43:48 -0700

    feat(issues)::sparkles: Auto request more info in issue with default … (#54)
    feat(issues)::sparkles: Auto request more info in issue with default title or empty body
    Fixes #47

--- src/utils/comment.util.ts
+++ src/utils/comment.util.ts
@@ -6,12 +6,12 @@ import type { Context } from "probot";
  * @param {Context} context Probot context
  * @param {string} comment The comment to create
  */
-export function createComment(context: Context, comment: string) {
+export async function createComment(context: Context, comment: string) {
 	const params = context.issue({
 		body: comment,
 	});
 
-	context.octokit.issues.createComment(params);
+	await context.octokit.issues.createComment(params);
 }
 
 /**
@@ -19,10 +19,10 @@ export function createComment(context: Context, comment: string) {
  *
  * @param {Context} context Probot context
  */
-export function deleteComment(context: Context<"issue_comment.deleted">) {
+export async function deleteComment(context: Context<"issue_comment.deleted">) {
 	const params = context.issue();
 
-	context.octokit.issues.deleteComment({
+	await context.octokit.issues.deleteComment({
 		...params,
 		comment_id: context.payload.comment.id,
 	});
