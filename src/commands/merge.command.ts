eda6cfea589ddc67a4069d1464daf6027b5ee810
Ayush Chugh <ayushchugh2006@gmail.com>
2022-09-05 14:18:13 +0530

    feat(comments): :sparkles: bot will comment on whose behalf a pr is approved or merged
    closes #38

--- src/commands/merge.command.ts
+++ src/commands/merge.command.ts
@@ -1,26 +1,19 @@
 import type { Context } from "probot";
 import { createComment } from "../utils/comment.util";
-import approveCommand from "./approve.command";
 
 async function mergeCommand(context: Context<"issue_comment.created">) {
 	const params = context.pullRequest();
 	const pullRequest = await context.octokit.pulls.get(params);
 
 	if (!pullRequest.data.mergeable) {
+		// @ts-ignore
 		return createComment(context, ":warning: Pull request is not mergeable.");
 	}
 
-	const reviewParams = context.pullRequest();
-
-	const reviews = await context.octokit.pulls.listReviews(reviewParams);
-
-	const approvedReviews = reviews.data.filter(
-		review => review.state === "APPROVED"
+	createComment(
+		context,
+		`Merging this pull_request as requested by @${context.payload.comment.user.login}`
 	);
-
-	if (approvedReviews.length === 0) {
-		approveCommand(context);
-	}
 
 	context.octokit.pulls.merge({
 		owner: params.owner,
